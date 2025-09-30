3e66a1f8c8ef2631ed36852db3d4318558681a97
Joaoof <joaodeus400@gmail.com>
2025-09-30 08:40:22 -0300

    fix: useCashMovement.ts

--- src/hooks/useCashMovements.ts
+++ src/hooks/useCashMovements.ts
@@ -1,13 +1,24 @@
-// /src/hooks/useCashMovements.ts
 import { useQuery } from '@apollo/client';
-import { GET_ALL_MOVEMENTS } from '../graphql/queries/queries';
+import { useAuth } from '../contexts/AuthContext';
+import { GET_CASH_MOVEMENTS } from '../graphql/queries/queries';
 
 export const useCashMovements = () => {
-    const { data, loading, error, refetch } = useQuery(GET_ALL_MOVEMENTS as any);
+    const { user, isLoading: isAuthLoading } = useAuth();
+
+    const variables = user?.id ? {
+        input: {
+            userId: user.id,
+        }
+    } : {};
+
+    const { data, loading, error, refetch } = useQuery(GET_CASH_MOVEMENTS, {
+        variables: variables,
+        skip: !user?.id || isAuthLoading,
+    });
 
     return {
-        movements: data?.entryMovement || [],
-        loading,
+        movements: data?.cashMovements || [],
+        loading: loading || isAuthLoading, // Considere o carregamento do AuthContext
         error,
         refetch,
     };
